604ee42e8ce5f5c9fc57d6c846d45c96c54d4319
Rico Lo <ka-lok.lo@ligo.org>
2020-10-03 13:40:06 -0700

    Update license information in setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
     ],
