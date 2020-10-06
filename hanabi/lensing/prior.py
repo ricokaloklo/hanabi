575be2254731ce58b993ed11c6e00c90543936b8
Rico Lo <ka-lok.lo@ligo.org>
2020-10-05 17:06:53 -0700

    Implement poor man's prior for relative magnification

--- hanabi/lensing/prior.py
+++ hanabi/lensing/prior.py
@@ -32,15 +32,39 @@ class DiscreteUniform(bilby.core.prior.Prior):
         return ((val >= self.minimum) & (val <= self.maximum))/float(self.N) * (np.modf(val)[0] == 0).astype(int)
 
     def cdf(self, val):
-        return (np.floor(val) - self.minimum + 1)/float(self.N) + (val > self.maximum)*(1 - (np.floor(val) - self.minimum + 1)/float(self.N))
+        return (val <= self.maximum) * (np.floor(val) - self.minimum + 1)/float(self.N) + (val > self.maximum)
 
 class RelativeMagnificationPoorMan(bilby.core.prior.Prior):
-    def __init__(self):
+    def __init__(self, name=None, latex_label=None, unit=None, maximum=np.inf):
         """
         RelativeMagnificationPoorMan
         ----------
 
-        Implement the poor man's prior for relative magnification
+        Implement the poor man's prior for relative magnification \mu_{rel}:
+        p(\mu_{rel}) = \frac{2 \mu_{rel, max}^2}{2 \mu_{rel, max}^2 - 1} * 
+        \begin{cases}
+        \mu_{rel} & \text{if } 0 < \mu_{rel} <= 1, \\
+        \mu_{rel}^{-3} & \text{if } 1 < \mu_{rel} <= \mu_{rel, max}, \\
+        0 & \text{otherwise}
+        \end{cases}
         """
-        pass
+        self.norm = 1.0/(1.0 - 1.0/(2.0 * np.power(maximum, 2)))
+        super(RelativeMagnificationPoorMan, self).__init__(
+            name=name,
+            latex_label=latex_label,
+            unit=unit,
+            minimum=0.,
+            maximum=maximum,
+            boundary=None
+        )
+
+    def rescale(self, val):
+        # Using generic inverse CDF method
+        return ((0 <= val) & (val < 0.5)) * np.sqrt(val/(self.norm*0.5)) + ((0.5 <= val) & (val < 1)) * np.sqrt(0.5/(1.0 - val/self.norm))
+
+    def prob(self, val):
+        return self.norm * (((0 < val) & (val <= 1)) * val + ((1 < val) & (val <= self.maximum)) * np.nan_to_num(1.0/np.power(val, 3)))
+
+    def cdf(self, val):
+        return self.norm * (((0 < val) & (val <= 1)) * 0.5*np.power(val, 2) + ((1 < val) & (val <= self.maximum)) * (1 - np.nan_to_num(0.5/np.power(val, 2)))) + (val > self.maximum)
 
