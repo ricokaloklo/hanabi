082bbf3d09df0437b922ca365f8c5296361b140b
Rico Lo <ka-lok.lo@ligo.org>
2020-10-07 17:34:18 -0700

    Refactor likelihood.py

--- hanabi/lensing/likelihood.py
+++ hanabi/lensing/likelihood.py
@@ -5,17 +5,12 @@ import bilby.gw.conversion
 import bilby.gw.likelihood
 from .waveform import *
 
-class LensingJointLikelihood(bilby.core.likelihood.Likelihood):
-    def __init__(self, single_trigger_likelihoods, lensed_waveform_model, sep_char="^", suffix=None):
+class JointLikelihood(bilby.core.likelihood.Likelihood):
+    def __init__(self, single_trigger_likelihoods, sep_char="^", suffix=None):
         # Initialize some variables using the default constructor
-        super(LensingJointLikelihood, self).__init__(parameters={})
+        super(JointLikelihood, self).__init__(parameters={})
         self.single_trigger_likelihoods = single_trigger_likelihoods
         self.n_triggers = len(self.single_trigger_likelihoods) # Reconstruct the number of triggers
-        self.lensed_waveform_model = lensed_waveform_model
-
-        # Assign the lensed waveform model specified to the single-trigger likelihoods
-        for single_trigger_likelihood in self.single_trigger_likelihoods:
-            single_trigger_likelihood.waveform_generator.frequency_domain_source_model = self.lensed_waveform_model
 
         self.sep_char = sep_char
         if suffix is None:
@@ -27,7 +22,6 @@ class LensingJointLikelihood(bilby.core.likelihood.Likelihood):
         if full_parameters is None:
             full_parameters = self.parameters
         
-        common_parameters = [p for p in full_parameters.keys() if self.sep_char not in p]
         parameters_per_trigger = []
 
         # Reconstruct the set of parameters that should have been passed to the likelihood
@@ -54,26 +48,6 @@ class LensingJointLikelihood(bilby.core.likelihood.Likelihood):
                     # In theory the object stored should be immutable, but just to be safe
                     trigger_parameters[new_name] = copy.deepcopy(full_parameters[old_name])
 
-            # Handle cases for lensing magnification
-            if "luminosity_distance" in common_parameters:
-                if "relative_magnification" in trigger_parameters.keys():
-                    # Overwrite the luminosity distance here to the apparent luminosity distance
-                    relative_magnification = trigger_parameters.pop("relative_magnification")
-                    trigger_parameters["luminosity_distance"] = trigger_parameters["luminosity_distance"]/(np.sqrt(relative_magnification))
-
-                    # NOTE We cannot get the source redshift here!
-
-                elif "absolute_magnification" in trigger_parameters.keys():
-                    # Overwrite the luminosity distance here to the apparent luminosity distance
-                    absolute_magnification = trigger_parameters.pop("absolute_magnification")
-                    source_luminosity_distance = trigger_parameters.pop("luminosity_distance")
-                    trigger_parameters["luminosity_distance"] = source_luminosity_distance/np.sqrt(absolute_magnification)
-                    # Convert source luminosity distance to source redshift in case needed
-                    trigger_parameters["redshift"] = bilby.gw.conversion.luminosity_distance_to_redshift(source_luminosity_distance)
-
-            if "redshift" in common_parameters:
-                raise NotImplementedError("The code does not support sampling over redshift yet")
-
             parameters_per_trigger.append(trigger_parameters)
         
         return parameters_per_trigger
@@ -95,3 +69,41 @@ class LensingJointLikelihood(bilby.core.likelihood.Likelihood):
     def noise_log_likelihood(self):
         # Sum over all the noise_log_likelihood values
         return np.sum([single_trigger_likelihood.noise_log_likelihood() for single_trigger_likelihood in self.single_trigger_likelihoods])
+
+class LensingJointLikelihood(JointLikelihood):
+    def __init__(self, single_trigger_likelihoods, lensed_waveform_model, sep_char="^", suffix=None):
+        super(LensingJointLikelihood, self).__init__(single_trigger_likelihoods, sep_char=sep_char, suffix=suffix)
+        self.lensed_waveform_model = lensed_waveform_model
+
+        # Assign the lensed waveform model specified to the single-trigger likelihoods
+        for single_trigger_likelihood in self.single_trigger_likelihoods:
+            single_trigger_likelihood.waveform_generator.frequency_domain_source_model = self.lensed_waveform_model
+
+    def assign_trigger_level_parameters(self, full_parameters=None):
+        common_parameters = [p for p in full_parameters.keys() if self.sep_char not in p]
+        parameters_per_trigger = super(LensingJointLikelihood, self).assign_trigger_level_parameters(full_parameters)
+
+        # Handle cases for lensing magnification
+        for trigger_idx in range(self.n_triggers):
+            trigger_parameters = parameters_per_trigger[trigger_idx]
+
+            if "luminosity_distance" in common_parameters:
+                if "relative_magnification" in trigger_parameters.keys():
+                    # Overwrite the luminosity distance here to the apparent luminosity distance
+                    relative_magnification = trigger_parameters.pop("relative_magnification")
+                    trigger_parameters["luminosity_distance"] = trigger_parameters["luminosity_distance"]/(np.sqrt(relative_magnification))
+
+                    # NOTE We cannot get the source redshift here!
+
+                elif "absolute_magnification" in trigger_parameters.keys():
+                    # Overwrite the luminosity distance here to the apparent luminosity distance
+                    absolute_magnification = trigger_parameters.pop("absolute_magnification")
+                    source_luminosity_distance = trigger_parameters.pop("luminosity_distance")
+                    trigger_parameters["luminosity_distance"] = source_luminosity_distance/np.sqrt(absolute_magnification)
+                    # Convert source luminosity distance to source redshift in case needed
+                    trigger_parameters["redshift"] = bilby.gw.conversion.luminosity_distance_to_redshift(source_luminosity_distance)
+
+            if "redshift" in common_parameters:
+                raise NotImplementedError("The code does not support sampling over redshift yet")
+        
+        return parameters_per_trigger
