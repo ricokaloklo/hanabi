313a44fd2394b6f2293091f1c4db5d1ca9bbc42e
Rico Lo <ka-lok.lo@ligo.org>
2020-10-15 10:34:19 -0700

    Pass new parameter conversion function to single likelihood

--- hanabi/lensing/likelihood.py
+++ hanabi/lensing/likelihood.py
@@ -4,6 +4,7 @@ import bilby.core.likelihood
 import bilby.gw.conversion
 import bilby.gw.likelihood
 from .waveform import *
+from .conversion import convert_to_lal_binary_black_hole_parameters_for_lensed_BBH
 
 class JointLikelihood(bilby.core.likelihood.Likelihood):
     def __init__(self, single_trigger_likelihoods, sep_char="^", suffix=None):
@@ -76,8 +77,10 @@ class LensingJointLikelihood(JointLikelihood):
         self.lensed_waveform_model = lensed_waveform_model
 
         # Assign the lensed waveform model specified to the single-trigger likelihoods
+        # FIXME For now we assign convert_to_lal_binary_black_hole_parameters_for_lensed_BBH as the default parameter_conversion function
         for single_trigger_likelihood in self.single_trigger_likelihoods:
             single_trigger_likelihood.waveform_generator.frequency_domain_source_model = self.lensed_waveform_model
+            single_trigger_likelihood.waveform_generator.parameter_conversion = convert_to_lal_binary_black_hole_parameters_for_lensed_BBH
 
     def assign_trigger_level_parameters(self, full_parameters=None):
         common_parameters = [p for p in full_parameters.keys() if self.sep_char not in p]
