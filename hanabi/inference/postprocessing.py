d5527510189bf32182d3cace39c65d175cecbcfa
Rico Lo <ka-lok.lo@ligo.org>
2020-10-14 17:41:37 -0700

    Change consistently from reweigh to reweight

--- hanabi/inference/postprocessing.py
+++ hanabi/inference/postprocessing.py
@@ -1,7 +1,8 @@
 import sys
 from bilby.core.result import Result
-from bilby.core.result import reweight as reweigh
-from bilby.gw.prior import convert_to_flat_in_component_mass_prior, UniformComovingVolume
+from bilby.core.result import reweight
+from bilby.gw.prior import convert_to_flat_in_component_mass_prior, UniformComovingVolume, PowerLaw
+from ..lensing.prior import RelativeMagnificationPoorMan
 from bilby_pipe.bilbyargparser import BilbyArgParser
 from bilby_pipe.utils import parse_args
 import configargparse
@@ -20,22 +21,32 @@ def create_parser(prog):
         "--flat-in-component-masses",
         action="store_true",
         default=False,
-        help="Reweigh the result to follow a flat prior in component masses"
+        help="Reweight the result to follow a flat prior in component masses"
     )
     parser.add(
         "--uniform-in-comoving-volume",
         action="store_true",
         default=False,
-        help="Reweigh the result to follow a uniform-in-comoving-volume prior in luminosity distance"
+        help="Reweight the result to follow a uniform-in-comoving-volume prior in luminosity distance"
+    )
+    parser.add(
+        "--convert-to-relative-magnification",
+        action="store_true",
+        default=False,
+        help=(
+            "Reweight the result with luminosity distances being sampled independently"
+            "to relative magnification following the poor man's prior."
+            "Note that the prior used during sampling must be PowerLaw with exponent of 2 (d_L^2)"
+        )
     )
 
     return parser
 
-def reweigh_flat_in_component_masses(result):
-    result_reweighed = convert_to_flat_in_component_mass_prior(result)
-    return result_reweighed
+def reweight_flat_in_component_masses(result):
+    result_reweighted = convert_to_flat_in_component_mass_prior(result)
+    return result_reweighted
 
-def reweigh_uniform_in_comoving_volume(result):
+def reweight_uniform_in_comoving_volume(result):
     # Read in the old priors
     old_priors = result.priors.copy()
 
@@ -51,9 +62,9 @@ def reweigh_uniform_in_comoving_volume(result):
                 latex_label=old_priors[name].latex_label
             )
 
-    result_reweighed = reweigh(result, old_prior=old_priors, new_prior=new_priors)
+    result_reweighted = reweight(result, old_prior=old_priors, new_prior=new_priors)
 
-    return result_reweighed
+    return result_reweighted
 
 def main():
     args, unknown_args = parse_args(sys.argv[1:], create_parser(__prog__))
@@ -62,12 +73,12 @@ def main():
     label = result.label
 
     if args.flat_in_component_masses:
-        result = reweigh_flat_in_component_masses(result)
+        result = reweight_flat_in_component_masses(result)
 
     if args.uniform_in_comoving_volume:
-        result = reweigh_uniform_in_comoving_volume(result)
+        result = reweight_uniform_in_comoving_volume(result)
 
     # Save to file
-    result.label = label + "_reweighed"
+    result.label = label + "_reweighted"
     result.save_to_file()
 
