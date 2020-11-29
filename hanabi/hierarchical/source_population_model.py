851fbf3274492e43d0ad0796b7df9471ecd7c801
Rico Lo <ka-lok.lo@ligo.org>
2020-11-28 16:02:20 -0800

    Rename SourcePopulationPrior to SourcePopulationModel

--- hanabi/hierarchical/source_population_model.py
+++ hanabi/hierarchical/source_population_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import bilby.gw.conversion
 import gwpopulation.models
 
-class SourcePopulationPrior(object):
+class SourcePopulationModel(object):
     def __init__(self, signal_parameter_names, population_parameter_dict):
         self.signal_parameter_names = signal_parameter_names
         self.population_parameter_dict = population_parameter_dict
@@ -32,7 +32,7 @@ class SourcePopulationPrior(object):
         return np.log(self.prob(dataset))
 
 
-class Marginalized(SourcePopulationPrior):
+class Marginalized(SourcePopulationModel):
     def __init__(self):
         super(Marginalized, self).__init__([], {})
 
@@ -49,7 +49,7 @@ class MarginalizedMergerRateDensity(Marginalized):
  
 
 # Wrapper for gwpopulation's power_law_primary_mass_ratio
-class PowerLawPrimaryMassRatio(SourcePopulationPrior):
+class PowerLawPrimaryMassRatio(SourcePopulationModel):
     def __init__(self, alpha, beta, mmin, mmax):
         super(PowerLawPrimaryMassRatio, self).__init__(
             signal_parameter_names=[
@@ -82,7 +82,7 @@ class PowerLawPrimaryMassRatio(SourcePopulationPrior):
             self.population_parameter_dict["mmax"]
         )
 
-class UniformAlignedSpinComponent(SourcePopulationPrior):
+class UniformAlignedSpinComponent(SourcePopulationModel):
     def __init__(self):
         super(UniformAlignedSpinComponent, self).__init__(
             signal_parameter_names=[
@@ -95,115 +95,3 @@ class UniformAlignedSpinComponent(SourcePopulationPrior):
     def prob(self, dataset):
         # z component from [-1, 1]
         return 0.25
-
-class MergerRateDensity(SourcePopulationPrior):
-    def _parameter_conversion(self, dataset):
-        if "luminosity_distance" in dataset.keys():
-            # Calculate redshift from luminosity distance
-            redshift = bilby.gw.conversion.luminosity_distance_to_redshift(
-                dataset["luminosity_distance"]
-            )
-        elif "comoving_distance" in dataset.keys():
-            # Calculate redshift from comoving distance
-            redshift = bilby.gw.conversion.comoving_distance_to_redshift(
-                dataset["comoving_distance"]
-            )
-        else:
-            raise ValueError("No distance measure in dataset")
-
-        dataset["redshift"] = redshift
-
-# Wrapper for gwpopulation's PowerLawRedshift
-class PowerLawRedshift(MergerRateDensity):
-    def __init__(self, R_0, kappa, redshift_max=2.3):
-        super(PowerLawRedshift, self).__init__(
-            signal_parameter_names=[
-                "redshift"
-            ],
-            population_parameter_dict={
-                'R_0': R_0,
-                'kappa': kappa,
-                'redshift_max': redshift_max
-            }
-        )
-
-        self._PowerLawRedshift = gwpopulation.models.redshift.PowerLawRedshift(
-            z_max=self.population_parameter_dict["redshift_max"]
-        )
-
-    def _prob(self, dataset):
-        return self._PowerLawRedshift.probability(
-            dataset=dataset, lamb=self.population_parameter_dict["kappa"]
-        )
-
-    def ln_dN_over_dz(self, dataset):
-        # NOTE This is not a normalized probability
-        return np.log(self.population_parameter_dict["R_0"] / 1e9) + np.log(
-            self._PowerLawRedshift.differential_spacetime_volume(
-                dataset=dataset, lamb=self.population_parameter_dict["kappa"]
-            )
-        )
-
-    def total_number_of_mergers(self, T_obs):
-        # R_0 is in the unit of Gpc^-3 T_obs^-1
-        # Since astropy.cosmology is using Mpc as the default unit
-        return self.population_parameter_dict["R_0"] / 1e9 * T_obs * \
-            self._PowerLawRedshift.normalisation(parameters={'lamb': self.population_parameter_dict["kappa"]})
-
-class AnalyticalBBHMergerRateDensity(MergerRateDensity):
-    def __init__(self, a_1, a_2, a_3, a_4, redshift_max):
-        super(AnalyticalBBHMergerRateDensity, self).__init__(
-            signal_parameter_names=['redshift'],
-            population_parameter_dict={
-                'a_1': a_1,
-                'a_2': a_2,
-                'a_3': a_3,
-                'a_4': a_4,
-                'redshift_max': redshift_max,
-            }
-        )
-
-        # The merger rate density fit has a unit of Gpc^-3 yr^-1
-        self.population_parameter_dict["R_0"] = self.evaluate_fit(0.0)
-
-    def evaluate_fit(self, z):
-        a_1 = self.population_parameter_dict['a_1']
-        a_2 = self.population_parameter_dict['a_2']
-        a_3 = self.population_parameter_dict['a_3']
-        a_4 = self.population_parameter_dict['a_4']
-        redshift_max = self.population_parameter_dict['redshift_max']
-
-        if 0. <= z < redshift_max:
-            return a_1*np.exp(a_2*z)/(a_4 + np.exp(a_3*z))
-        else:
-            return 0.0
-
-class BelczynskiEtAl2017PopIPopIIStarsBBHMergerRateDensity(AnalyticalBBHMergerRateDensity):
-    def __init__(self):
-        super(BelczynskiEtAl2017PopIPopIIStars, self).__init__(
-            6.6e3,
-            1.6,
-            2.1,
-            30,
-            15
-        )
-
-class BelczynskiEtAl2017PopIIIStarsBBHMergerRateDensity(AnalyticalBBHMergerRateDensity):
-    def __init__(self):
-        super(BelczynskiEtAl2017PopIIIStars, self).__init__(
-            6e4,
-            1.0,
-            1.4,
-            3e6,
-            45
-        )
-
-class KinugawaEtAl2016PopIIIStarsBBHMergerRateDensity(AnalyticalBBHMergerRateDensity):
-    def __init__(self):
-        super(KinugawaEtAl2016PopIIIStars, self).__init__(
-            1e4,
-            0.7,
-            1.1,
-            500,
-            45
-        )   
