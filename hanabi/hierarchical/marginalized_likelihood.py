ed222f34965a39ddfb9868ccc2b2a5ae914e0a3c
Rico Lo <ka-lok.lo@ligo.org>
2020-12-02 23:26:42 -0800

    Implement two priors for reweighting

--- hanabi/hierarchical/marginalized_likelihood.py
+++ hanabi/hierarchical/marginalized_likelihood.py
@@ -1,5 +1,67 @@
 import bilby
+from .utils import get_ln_weights_for_reweighting
 from bilby.core.likelihood import Likelihood
+from bilby.core.prior import Prior, PriorDict
+
+class LuminosityDistancePriorFromAbsoluteMagnificationRedshift(Prior):
+    def __init__(self, abs_magnification_prob_dist, z_src, name=None, latex_label=None, unit=None):
+        super(LuminosityDistancePriorFromAbsoluteMagnificationRedshift, self).__init__(
+            name=name,
+            latex_label=latex_label,
+            unit=unit,
+        )
+        self.abs_magnification_prob_dist = abs_magnification_prob_dist
+        self.z_src = z_src
+        self.d_L_src = bilby.gw.conversion.redshift_to_luminosity_distance(z_src)
+
+    def Jacobian(self, d_L):  
+        return 2.0 * (self.d_L_src/d_L)**2 * (1./d_L)
+
+    def prob(self, d_L):
+        mu_abs = (self.d_L_src/d_L)**2
+        return self.abs_magnification_prob_dist.prob(mu_abs)*self.Jacobian(d_L)
+
+# NOTE This is not a full-blown bilby PriorDict but it does the job!
+class DetectorFrameComponentMassesFromSourceFrame(dict):
+    def __init__(self, mass_src_pop_model, z_src):
+        self.mass_src_pop_model = mass_src_pop_model
+        self.z_src = z_src
+
+    def Jacobian(self):
+        return np.power((1. + self.z_src), -2)
+
+    def prob(self, dataset):
+        mass_1_det = dataset["mass_1"]
+        mass_2_det = dataset["mass_2"]
+
+        mass_1_src = mass_1_det / (1. + self.z_src)
+        mass_2_src = mass_2_det / (1. + self.z_src)
+
+        return self.mass_src_pop_model.prob({'mass_1_source': mass_1_src, 'mass_2_source': mass_2_src})
+
+    def ln_prob(self, dataset):
+        return np.log(self.prob(dataset))
 
 class MonteCarloMarginalizedLikelihood(Likelihood):
-    pass
+    def __init__(self, result, mass_src_pop_model, spin_src_pop_model, abs_magnification_prob_dists, sep_char="^", suffix=None):
+        # The likelihood is a function of the source redshift only
+        # Might as well do this marginalization deterministically
+        self.parameters = {'redshift': 0.0}
+        self.result = result
+        self.mass_src_pop_model = mass_src_pop_model
+        self.spin_src_pop_model = spin_src_pop_model
+        # This should be a list of abs_magnification_prob_dist
+        self.abs_magnification_prob_dists = abs_magnification_prob_dists
+
+        self.sep_char = sep_char
+        if suffix is None:
+            self.suffix = lambda trigger_idx: "{}({})".format(self.sep_char, trigger_idx + 1)
+        else:
+            self.suffix = suffix
+
+    def compute_ln_weights_for_luminosity_distances(self, z_src):
+        for trigger_idx, abs_magnification_prob_dist in enumerate(self.abs_magnification_prob_dists):
+            pass
+
+    def log_likelihood(self):
+        z_src = self.parameters["redshift"]
