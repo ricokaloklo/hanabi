8c1e7f3c04d818d19f3c71d1308c1d1087159aaf
Rico Lo <ka-lok.lo@ligo.org>
2020-12-02 19:34:12 -0800

    Return ln_weights instead

--- hanabi/hierarchical/utils.py
+++ hanabi/hierarchical/utils.py
@@ -3,7 +3,7 @@ import bilby
 
 # This is a stripped-down version of bilby.core.result.get_weights_for_reweighting
 # Currently the function in bilby v1.0.2 is unusable if rejection sampling was used before
-def get_weights_for_reweighting(result, old_priors, new_priors, parameters):
+def get_ln_weights_for_reweighting(result, old_priors, new_priors, parameters):
     n_posteriors = len(result.posterior)
     old_log_prior_array = np.zeros(n_posteriors)
     new_log_prior_array = np.zeros(n_posteriors)
@@ -14,4 +14,4 @@ def get_weights_for_reweighting(result, old_priors, new_priors, parameters):
         new_log_prior_array[idx] = new_priors.ln_prob(par_sample)
 
     ln_weights = new_log_prior_array - old_log_prior_array
-    return np.exp(ln_weights)
+    return ln_weights
