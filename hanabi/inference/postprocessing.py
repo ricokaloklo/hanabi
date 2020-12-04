7f5777ca8afdc90261e7a631aab32315c88b0450
Rico Lo <ka-lok.lo@ligo.org>
2020-12-03 21:15:42 -0800

    Fix hanabi_postprocess_result

--- hanabi/inference/postprocessing.py
+++ hanabi/inference/postprocessing.py
@@ -1,11 +1,13 @@
 import sys
 from bilby.core.result import Result
 from bilby.core.result import reweight
-from bilby.gw.prior import convert_to_flat_in_component_mass_prior, UniformComovingVolume, PowerLaw
+from bilby.gw.prior import convert_to_flat_in_component_mass_prior, UniformComovingVolume
 from ..lensing.prior import RelativeMagnificationPoorMan
+from ..lensing.conversion import convert_to_lal_binary_black_hole_parameters_for_lensed_BBH
 from bilby_pipe.bilbyargparser import BilbyArgParser
 from bilby_pipe.utils import parse_args
 import configargparse
+import logging
 
 __prog__ = "hanabi_postprocess_result"
 
@@ -33,6 +35,12 @@ def create_parser(prog):
     return parser
 
 def reweight_flat_in_component_masses(result):
+    logger = logging.getLogger(__prog__)
+    if not all([k in result.posterior.columns for k in ["mass_1", "mass_2"]]):
+        # Missing mass_1 mass_2 columns
+        converted_posterior, added_keys = convert_to_lal_binary_black_hole_parameters_for_lensed_BBH(result.posterior)
+        result.posterior = converted_posterior
+        logger.info("Adding {} samples".format(", ".join(added_keys)))
     result_reweighted = convert_to_flat_in_component_mass_prior(result)
     return result_reweighted
 
@@ -70,5 +78,5 @@ def main():
 
     # Save to file
     result.label = label + "_reweighted"
-    result.save_to_file()
+    result.save_to_file(outdir=".")
 
