956f2e39935aedcd44be6a120848fea558ad1438
Rico Lo <ka-lok.lo@ligo.org>
2020-12-03 22:43:04 -0800

    Lightning fast get_ln_weights_for_reweighting

--- hanabi/hierarchical/utils.py
+++ hanabi/hierarchical/utils.py
@@ -8,10 +8,9 @@ def get_ln_weights_for_reweighting(result, old_priors, new_priors, parameters):
     old_log_prior_array = np.zeros(n_posteriors)
     new_log_prior_array = np.zeros(n_posteriors)
 
-    for idx, (_, sample) in enumerate(result.posterior.iterrows()):
-        par_sample = {key: sample[key] for key in parameters}
-        old_log_prior_array[idx] = old_priors.ln_prob(par_sample)
-        new_log_prior_array[idx] = new_priors.ln_prob(par_sample)
+    par_samples = {key: result.posterior[key] for key in parameters}
+    old_log_prior_array = old_priors.ln_prob(par_samples)
+    new_log_prior_array = new_priors.ln_prob(par_samples)
 
     ln_weights = new_log_prior_array - old_log_prior_array
     return ln_weights
