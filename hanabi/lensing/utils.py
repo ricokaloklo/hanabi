61c5e0d978b6d16ad9f4bd1b7120fb67f27b2731
Rico Lo <ka-lok.lo@ligo.org>
2020-12-07 15:26:48 -0800

    Change parameter_suffix to a class

--- hanabi/lensing/utils.py
+++ hanabi/lensing/utils.py
@@ -1,2 +1,6 @@
-def parameter_suffix(trigger_idx, sep_char):
-    return "{}({})".format(sep_char, trigger_idx + 1)
+class ParameterSuffix(object):
+    def __init__(self, sep_char="^"):
+        self.sep_char = sep_char
+
+    def __call__(self, trigger_idx):
+        return "{}({})".format(self.sep_char, trigger_idx + 1)
