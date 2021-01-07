c5b3e6d5e9a69d5bf1687b854b27426be5210929
Rico Lo <ka-lok.lo@ligo.org>
2021-01-06 22:32:23 -0800

    Restructure p_z.py

--- hanabi/hierarchical/p_z.py
+++ hanabi/hierarchical/p_z.py
@@ -8,17 +8,16 @@ from .merger_rate_density import MergerRateDensity, MarginalizedMergerRateDensit
 from ..lensing.optical_depth import *
 from ..lensing.absolute_magnification import *
 
-class LensedSourceRedshiftProbDist(Interped):
-    def __init__(self, merger_rate_density, optical_depth, name=None, latex_label=None):
+class SourceRedshiftProbDist(Interped):
+    def __init__(self, merger_rate_density, name=None, latex_label=None):
         self.redshift_max = merger_rate_density.population_parameter_dict["redshift_max"]
         self.merger_rate_density = merger_rate_density
-        self.optical_depth = optical_depth
 
         self.zs = np.linspace(0., self.redshift_max, num=1000)
         self.compute_normalization()
         self.pdfs = self._prob(self.zs)
 
-        super(LensedSourceRedshiftProbDist, self).__init__(
+        super(SourceRedshiftProbDist, self).__init__(
             self.zs,
             self.pdfs,
             minimum=0.,
@@ -40,12 +39,24 @@ class LensedSourceRedshiftProbDist(Interped):
         return interped_prior.to_json()
 
     def _prob(self, z):
-        return (1.0/self.normalization) * self.optical_depth.evaluate(z) * self.merger_rate_density.prob({"redshift": z})
+        return (1.0/self.normalization) * self.merger_rate_density.prob({"redshift": z})
 
     def compute_normalization(self):
         self.normalization = 1.0
         norm = scipy.integrate.simps(self._prob(self.zs), self.zs)
         self.normalization = norm
+
+class LensedSourceRedshiftProbDist(SourceRedshiftProbDist):
+    def __init__(self, merger_rate_density, optical_depth, name=None, latex_label=None):
+        self.optical_depth = optical_depth
+        super(LensedSourceRedshiftProbDist, self).__init__(
+            merger_rate_density=merger_rate_density,
+            name=name,
+            latex_label=latex_label,
+        )
+
+    def _prob(self, z):
+        return (1.0/self.normalization) * self.optical_depth.evaluate(z) * self.merger_rate_density.prob({'redshift': z})
 
 class NotLensedSourceRedshiftProbDist(LensedSourceRedshiftProbDist):
     def _prob(self, z):
