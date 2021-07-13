0581f80d0f6e64b854c00795006b27a02b986df6
Rico Ka Lok Lo <ka-lok.lo@ligo.org>
2021-07-13 18:17:53 -0500

    Not checking consistency of calibration-related parameters in waveform cache

--- hanabi/lensing/likelihood.py
+++ hanabi/lensing/likelihood.py
@@ -64,17 +64,17 @@ class LensingJointLikelihoodWithWaveformCache(LensingJointLikelihood):
     
     def add_to_cache(self, likelihood, parameters):
         self._cache = copy.deepcopy(likelihood.waveform_generator._cache)
-        self._cache["sampled_parameters"] = parameters
+        self._cache["waveform_parameters"] = {k:v for k,v in parameters.items() if not k.startswith("recalib_")}
 
     def transform_from_cache(self, likelihood, parameters):
         # Check if the cached waveform can be transformed to the desired waveform
         # The following parameters can be (and should be) different
         # FIXME This will break if sampled in detector-frame time
         parameters_excluded_from_comparison = ["image_type", "luminosity_distance", "geocent_time"]
-        parameters_to_be_checked = [k for k in parameters.keys() if k not in parameters_excluded_from_comparison]
-        if {k:v for k,v in parameters.items() if k in parameters_to_be_checked} == {k:v for k,v in self._cache["sampled_parameters"].items() if k in parameters_to_be_checked}:
-            scale = self._cache["sampled_parameters"]["luminosity_distance"]/parameters["luminosity_distance"]
-            phase = morse_phase_from_image_type(int(parameters["image_type"])) - morse_phase_from_image_type(int(self._cache["sampled_parameters"]["image_type"]))
+        parameters_to_be_checked = [k for k in parameters.keys() if k not in parameters_excluded_from_comparison and not k.startswith("recalib_")]
+        if {k:v for k,v in parameters.items() if k in parameters_to_be_checked} == {k:v for k,v in self._cache["waveform_parameters"].items() if k in parameters_to_be_checked}:
+            scale = self._cache["waveform_parameters"]["luminosity_distance"]/parameters["luminosity_distance"]
+            phase = morse_phase_from_image_type(int(parameters["image_type"])) - morse_phase_from_image_type(int(self._cache["waveform_parameters"]["image_type"]))
 
             # Perform the transformation
             self._cache["waveform"]["plus"] = np.exp(-1j*phase)*np.ones_like(self._cache["waveform"]["plus"])*scale*self._cache["waveform"]["plus"]
