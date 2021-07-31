07e072527fbd80842cef1aa83f39a2ccb98b5a4b
Rico Ka Lok Lo <ka-lok.lo@ligo.org>
2021-07-31 03:09:13 -0500

    Convert pandas series to numpy array

--- hanabi/hierarchical/utils.py
+++ hanabi/hierarchical/utils.py
@@ -27,7 +27,7 @@ def enforce_mass_ordering(m1, m2):
 # This is a stripped-down version of bilby.core.result.get_weights_for_reweighting
 # Currently the function in bilby v1.0.2 is unusable if rejection sampling was used before
 def get_ln_weights_for_reweighting(result, old_priors, new_priors, parameters):
-    samples = {key: result.posterior[key] for key in parameters}
+    samples = {key: result.posterior[key].to_numpy() for key in parameters}
     old_log_prior_array = old_priors.ln_prob(samples, axis=0)
     new_log_prior_array = new_priors.ln_prob(samples, axis=0)
 
