e7d575b79615779aefecb1b0456388c057260997
Rico Lo <ka-lok.lo@ligo.org>
2021-09-28 18:32:14 -0700

    Relax package requirements

--- setup.py
+++ setup.py
@@ -82,11 +82,12 @@ setuptools.setup(
         ]
     },
     install_requires=[
-        "bilby==1.0.2",
-        "bilby_pipe==1.0.2",
+        "bilby>=1.0.2",
+        "bilby_pipe>=1.0.2",
         "gwpopulation",
-        "parallel_bilby==0.1.5",
+        "parallel_bilby>=0.1.5",
         "mpi4py",
+        "configargparse==1.4",
     ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
