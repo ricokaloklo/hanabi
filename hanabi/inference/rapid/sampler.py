f961c478af54f9a8d9fc53212e6e0dd314f62bd6
Rico Lo <ka-lok.lo@ligo.org>
2022-01-18 00:21:34 -0800

    Add routine to 'refine' samples with MCMC

--- hanabi/inference/rapid/sampler.py
+++ hanabi/inference/rapid/sampler.py
@@ -137,3 +137,34 @@ def generate_all_parameters(
         {"log_likelihood": joint_log_Ls[drawn_image_types]}
     )
     return posterior
+
+def lnpriorfn(x, bilby_prior, joint_search_parameter_keys):
+    theta_dict = {k: x[idx] for idx, k in enumerate(joint_search_parameter_keys)}
+    # NOTE The image_type parameters are *discrete*, need to apply transformation
+    for p in theta_dict.keys():
+        if p.startswith("image_type"):
+            theta_dict[p] = round(theta_dict[p])
+            
+    log_prior = bilby_prior.ln_prob(theta_dict)
+    return log_prior
+
+def lnlikefn(x, bilby_likelihood, joint_search_parameter_keys):
+    theta_dict = {k: x[idx] for idx, k in enumerate(joint_search_parameter_keys)}
+    # NOTE The image_type parameters are *discrete*, need to apply transformation
+    for p in theta_dict.keys():
+        if p.startswith("image_type"):
+            theta_dict[p] = round(theta_dict[p])
+
+    bilby_likelihood.parameters.update(theta_dict)
+    return bilby_likelihood.log_likelihood()
+
+def lnpostfn(x, bilby_likelihood, bilby_prior, joint_search_parameter_keys):
+    log_prior = lnpriorfn(x, bilby_prior, joint_search_parameter_keys)
+    if not np.isfinite(log_prior):
+        return -np.inf
+    else:
+        log_like = lnlikefn(x, bilby_likelihood, joint_search_parameter_keys)
+        if not np.isfinite(log_like):
+            return -np.inf
+        else:
+            return log_like + log_prior
