00e552d988667792e81a3f6a9f732f6b93cdab79
Rico Lo <ka-lok.lo@ligo.org>
2022-01-20 23:25:43 -0800

    Not use MCMC to refine samples

--- hanabi/inference/rapid/rapid_analysis.py
+++ hanabi/inference/rapid/rapid_analysis.py
@@ -366,7 +366,7 @@ class ConditionalInference():
             )
 
         # Generate samples for the common parameters by reweighting
-        _n_iterations = 1000
+        _n_iterations = 10000
         joint_posterior_samples = None
 
         logger.info("Reweighting posterior samples")
@@ -407,7 +407,6 @@ class ConditionalInference():
 
     def regenerate_joint_posterior_samples_from_mcmc(self, samples):
         theta_to_evaluate = samples[self.joint_search_parameter_keys]
-        log_priors = self.joint_priors.ln_prob(theta_to_evaluate, axis=0)
 
         if self.waveform_cache:
             joint_likelihood = LensingJointLikelihoodWithWaveformCache(self.single_trigger_likelihoods, sep_char=self.sep_char, suffix=self.suffix)
@@ -418,25 +417,13 @@ class ConditionalInference():
 
         _mcmc_sampler_kwargs = {
             "nwalkers": 64,
-            "iterations": 500,
+            "iterations": 2500,
         }
         _mcmc_sampler_kwargs.update(self.mcmc_sampler_kwargs)
         ndim = len(self.joint_search_parameter_keys)
         nwalkers = _mcmc_sampler_kwargs["nwalkers"]
 
-        image_type_pnames = sorted([p for p in self.joint_search_parameter_keys if p.startswith("image_type")])
-        possible_image_type_combos = samples.groupby(image_type_pnames)
-        #logger.info("Possible combinations of the image types:")
-        image_type_combo_counts = possible_image_type_combos.size()
-        #print(image_type_combo_counts.div(image_type_combo_counts.sum()))
-        chunks = np.array_split(np.arange(nwalkers), len(possible_image_type_combos.groups))
-
-        # Seed the walkers
-        p0 = None
-        for i, (combo, row_indices) in enumerate(possible_image_type_combos.groups.items()):
-            for c_idx, j in enumerate(chunks[i]):
-                p0 = pd.concat((p0, theta_to_evaluate.iloc[row_indices].sample()))
-
+        p0 = theta_to_evaluate[self.joint_search_parameter_keys].sample(nwalkers).to_numpy()
         logger.info("Launching MCMC for joint posterior samples")
         import zeus
 
@@ -532,8 +519,6 @@ class ConditionalInference():
         joint_posterior_samples = None
         if self.generate_joint_posterior_samples:
             joint_posterior_samples = self.generate_joint_posterior_samples()
-            # Refine posterior samples with MCMC
-            joint_posterior_samples = self.regenerate_joint_posterior_samples_from_mcmc(joint_posterior_samples)
 
         # Save to file
         joint_result = bilby.core.result.Result(
